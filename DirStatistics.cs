99680ab2bd79453e998e57fe04dc6b9abeac6b37
Yulia Gvardeeva <julia.athen@yandex.ru>
2022-03-30 21:23:35 +0300

    delete all and start again

--- DirStatistics.cs
+++ DirStatistics.cs
@@ -6,60 +6,37 @@ namespace DirStat
 {
     internal class DirStatistics
     {
-        private string DirPath;
+        private DirectoryInfo DirInfo;
+        private List<FileStatItem> FileStatItems;
+        private string StatFilePath;
         public DirStatistics(string path)
         {
-            DirPath = path;
-        }
-        public List<FileInfo> FilesSortedBySizeDesc()
-        {
-            var files = new List<FileInfo>();
-            var paths = Directory.GetFiles(DirPath);
-            foreach (var path in paths)
-            {
-                files.Add(new FileInfo(path));  
-            }
-            var comparer = new BySizeComparerDesc();
-            files.Sort(comparer);
-            return files;
-        }
-        public List<FileInfo> FilesSortedByCreateTime()
-        {
-            var files = new List<FileInfo>();
-            var paths = Directory.GetFiles(DirPath);
-            foreach (var path in paths)
-            {
-                files.Add(new FileInfo(path));
-            }
-            var comparer = new ByCreateTimeComparer();
-            files.Sort(comparer);
-            return files;
-        }
-        public List<KeyValuePair<string, int>> ExtensionAndFrequencyInfo()
-        {
-            var paths = Directory.GetFiles(DirPath);
-            var extFrequency = new Dictionary<string, int>();
-            foreach (var path in paths)
-            {
-                var currExt = Path.GetExtension(path);
-                if (!extFrequency.ContainsKey(currExt))
-                    extFrequency.Add(currExt, 1);
-                else
-                    extFrequency[currExt] = extFrequency[currExt] + 1;
-            }
-            var sortedList = extFrequency.ToList();
-            sortedList.Sort(CompareByValueDesc);  
-            return sortedList;
+            DirInfo = new DirectoryInfo(path);
+            FileStatItems = new List<FileStatItem>();
+            StatFilePath = "";
         }
 
-        private int CompareByValueDesc(KeyValuePair<string, int> x, KeyValuePair<string, int> y)
+        public List<FileStatItem> GetFileStatItems()
         {
-            if (x.Value < y.Value)
-                return 1;
-            else if (x.Value == y.Value)
-                return 0;
-            else
-                return -1;
+            if (FileStatItems.Count == 0) {
+                FillStatistics();
+            }
+            return FileStatItems;   
+        }
+        private void FillStatistics()
+        {
+            FillStatisticsFromFile();
+            if (FileStatItems.Count == 0) {
+                RecursiveWalkDir();
+            }
+        }
+        private void FillStatisticsFromFile()
+        {
+
+        }
+        private void RecursiveWalkDir()
+        {
+
         }
     }
 }
