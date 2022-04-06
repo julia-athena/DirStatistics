42d95f6457fed2e252012d3afca9d8dfd5cfdd38
Yulia Gvardeeva <julia.athen@yandex.ru>
2022-04-06 08:40:06 +0300

    show in console result

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ Console.WriteLine("Enter directory path to get statistics");
 var path = Console.ReadLine();
 var stat = new DirStatistics(path);
 
-var topbig = stat.GetTopNFiles(3, new BySizeComparerDesc());
-var p = Console.ReadLine();
+var files = stat.GetTopNFiles(3, new BySizeComparerDesc());
+var table = new ConsoleTable("FileName", "Size");
+for (int i = 0; i < files.Count; i++)
+{
+    table.AddRow(files[i].FullName, files[i].Size.ToString());
+}
+table.Write();
 stat.FreshStatistics();
