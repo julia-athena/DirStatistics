ea9e1219d213b104f3fe17584597c562a9fdd270
Yulia Gvardeeva <julia.athen@yandex.ru>
2022-04-28 21:36:19 +0300

    Add model for params of request

---
+++ WebDirStat/Models/StatOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDirStat.Models
+{
+    public class StatOptions
+    {
+        public string Dir { get; set; }
+        public string Sort { get; set; }
+        public int? Top { get; set; }
+    }
+}
